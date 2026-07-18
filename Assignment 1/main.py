574dd76e2c99a5e1921d60744cf9533e7eaf8947
parth5012 <parthchawla5012@GMAIL.COM>
2026-07-18 11:46:52 +0530

    Completed Stage 1

---
+++ Assignment 1/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }
+
+@app.get("/health")
+def health():
+    return { "status": "ok" }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
