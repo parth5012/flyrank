bd1736fbd7ac4e306378b2c1839b8436bb3539ba
parth5012 <parthchawla5012@GMAIL.COM>
2026-07-27 11:13:30 +0530

    Stage 4: explored SQLite

--- Assignment 2/db/models.py
+++ Assignment 2/db/models.py
@@ -2,6 +2,7 @@ from sqlmodel import SQLModel, Field
 
 
 class Task(SQLModel, table=True):
+    __tablename__ = "tasks"
     id: int = Field(primary_key=True, default=None)
     title: str = Field()
     done: bool = Field(default=False)
